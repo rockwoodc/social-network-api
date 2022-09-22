57a69482d4a8fed1c266d94fe27f075f27144d31
rockwoodc <rockwoodce@gmail.com>
2022-09-22 17:15:00 -0400

    correct file structure

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 //this file will package up all of the models
-const thought = require('./thoughts');
-const user = require('./users');
+const thought = require('./Thought');
+const user = require('./User');
 
 module.exports = { thought, user };
