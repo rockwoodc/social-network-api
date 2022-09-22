ee3374c880c1ae82555841fdf0203a97df4d7bab
rockwoodc <rockwoodce@gmail.com>
2022-09-22 16:56:53 -0400

    thought/reaction schema

--- models/thought.js
+++ models/thought.js
@@ -1,2 +1,69 @@
-const { Schema, model, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent thought _id
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        max: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+      },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,  
+      getters: true
+    },
+  }
+);
+
+// get total count of replies on retrieval
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reaction.length;
+  });
+
+
+
+const thought = model("thought", thoughtSchema);
+
+module.exports = thought;
